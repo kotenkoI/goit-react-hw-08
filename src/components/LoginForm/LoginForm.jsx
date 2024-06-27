6eefa0d62a3ea21f27ac2bf23e7567e76c4559d2
kotenkoI <iryna.herashchenko@gmail.com>
2024-06-27 17:14:07 +0300

    added

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { logIn } from "../../redux/auth/operations";
 import css from "./LoginForm.module.css";
 import { useId } from "react";
-// import * as Yup from "yup";
+import * as Yup from "yup";
 export default function LoginForm() {
   const dispatch = useDispatch();
 
@@ -21,9 +21,6 @@ const validationControl = Yup.object().shape({
 
   const handleSubmit = (values, actions) => {
     dispatch(logIn(values))
-  //  .unwrap()
-  //   .then()
-  //   .catch(error =>  toast("The contact has been added") )
  
     
     actions.resetForm();
